29e859e867b128976b85ab0e34face95a242dc27
JoahvanderSloot <144343776+JoahvanderSloot@users.noreply.github.com>
2024-10-15 17:34:14 +0200

    generic singleton bug fixed

--- Assets/ScriptebleObject/GenericScriptableSingleton.cs
+++ Assets/ScriptebleObject/GenericScriptableSingleton.cs
@@ -1,3 +1,7 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 using UnityEngine;
 
 public class GenericScriptableSingleton<T> : ScriptableObject where T : ScriptableObject
@@ -10,25 +14,30 @@ public class GenericScriptableSingleton<T> : ScriptableObject where T : Scriptab
         {
             if (m_instance == null)
             {
-                // Find an existing instance in the project (optional)
-                m_instance = Resources.FindObjectsOfTypeAll<T>()[0];
-
-                // If not found, create a new instance
+                // Try to find the asset in the project
+                T[] assets = Resources.FindObjectsOfTypeAll<T>();
+                if (assets.Length > 0)
+                {
+                    m_instance = assets[0];
+                }
+                #if UNITY_EDITOR
+                else
+                {
+                    string[] guids = AssetDatabase.FindAssets("t:" + typeof(T).Name);
+                    if (guids.Length > 0)
+                    {
+                        string assetPath = AssetDatabase.GUIDToAssetPath(guids[0]);
+                        m_instance = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+                    }
+                }
+                #endif
+                // If still not found, create a new instance
                 if (m_instance == null)
                 {
                     m_instance = CreateInstance<T>();
                 }
             }
             return m_instance;
-        }
-    }
-
-    // Optional: if needed, use Awake for initialization logic
-    protected virtual void OnEnable()
-    {
-        if (m_instance == null)
-        {
-            m_instance = this as T;
         }
     }
 }
